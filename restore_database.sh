2686c5b7944bde0486c344caa934cb03108faded
Diego Hordi <diego.hordi@gmail.com>
2020-01-15 06:45:22 +0000

    Initial commit

--- restore_database.sh
+++ restore_database.sh
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env bash
 echo "Restoring database"
 psql -U postgres -c "CREATE DATABASE ${DBNAME}"
-pg_restore -v -d esus /tmp/${FILE} > /tmp/log
+pg_restore -v -d ${DBNAME} /tmp/${FILE} > /tmp/log
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE ${DBNAME} TO postgres"
 echo "Database restored successfully"
